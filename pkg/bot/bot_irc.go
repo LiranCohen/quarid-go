bf9e2fac25b492e8a7e48fa0dbf5035b4463a463
lirancohen <lirancohen@me.com>
2015-11-09 01:09:11 -0500

    made quarid exportable

--- pkg/bot/bot_irc.go
+++ pkg/bot/bot_irc.go
@@ -1,11 +1,8 @@
 package bot
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
-	"regexp"
-	"strings"
 
 	"github.com/enmand/quarid-go/pkg/adapter"
 	"github.com/enmand/quarid-go/pkg/config"
@@ -16,21 +13,12 @@ import (
 	"github.com/enmand/quarid-go/vm/js"
 )
 
-type quarid struct {
+type Quarid struct {
 	// Connection to the IRC server
 	IRC *irc.Client
 
 	// Configuration from the user
 	Config *config.Config
-
-	// List of Opers
-	Opers map[string]struct{}
-
-	//List of Channel Ops
-	ChanOps map[string][]string // map[channel][]mask
-
-	//List of Channel Ops
-	ChanAdmins map[string][]string // map[channel][]mask
 
 	// The Plugins we have loaded
 	plugins []plugin.Plugin
@@ -39,7 +27,7 @@ type quarid struct {
 	vms map[string]vm.VM
 }
 
-func (q *quarid) initialize() error {
+func (q *Quarid) initialize() error {
 	q.IRC = irc.NewClient(
 		q.Config.GetString("irc.nick"),
 		q.Config.GetString("irc.user"),
@@ -58,12 +46,6 @@ func (q *quarid) initialize() error {
 	q.vms = map[string]vm.VM{
 		vm.JS: js.NewVM(),
 	}
-	q.Opers = make(map[string]struct{})
-	q.ChanOps = make(map[string][]string)
-	admins := q.Config.GetStringSlice("irc.admins")
-	for _, admin := range admins {
-		q.Opers[admin] = struct{}{}
-	}
 
 	var errs []error
 	q.plugins, errs = q.LoadPlugins(q.Config.GetStringSlice("plugins_dirs"))
@@ -81,7 +63,7 @@ func (q *quarid) initialize() error {
 	return nil
 }
 
-func (q *quarid) LoadPlugins(dirs []string) ([]plugin.Plugin, []error) {
+func (q *Quarid) LoadPlugins(dirs []string) ([]plugin.Plugin, []error) {
 	var ps []plugin.Plugin
 	var errs []error
 
@@ -110,284 +92,7 @@ func (q *quarid) LoadPlugins(dirs []string) ([]plugin.Plugin, []error) {
 	return ps, errs
 }
 
-func getNick(mask string) string {
-	split := strings.Split(mask, "@")
-	if len(split) > 0 {
-		ident := strings.Split(mask, "!")
-		if len(ident) > 0 {
-			return ident[0]
-		}
-	}
-	return ""
-}
-
-type HostMask struct {
-	Nick string
-	User string
-	Host string
-}
-
-func FormatMask(mask string) (HostMask, error) {
-	hm := HostMask{}
-	hostSplit := strings.Split(mask, "@")
-	if len(hostSplit) > 1 {
-		preSplit := strings.Split(hostSplit[0], "!")
-		hm.Host = hostSplit[1]
-		if len(preSplit) > 1 {
-			hm.Nick = preSplit[0]
-			hm.User = preSplit[1]
-			return hm, nil
-		} else if len(preSplit) > 0 && preSplit[0] == "*" {
-			hm.Nick = "*"
-			hm.User = "*"
-			return hm, nil
-		}
-	}
-	return hm, errors.New("invalid mask")
-}
-
-//Taken from github.com/edmund-huber/ergonomadic
-// Generate a regular expression from the set of user mask
-// strings. Masks are split at the two types of wildcards, `*` and
-// `?`. All the pieces are meta-escaped. `*` is replaced with `.*`,
-// the regexp equivalent. Likewise, `?` is replaced with `.`. The
-// parts are re-joined and finally all masks are joined into a big
-// or-expression.
-func maskRegexp(masks []string) *regexp.Regexp {
-	rReg := &regexp.Regexp{}
-	if len(masks) == 0 {
-		rReg = nil
-		return rReg
-	}
-
-	maskExprs := make([]string, len(masks))
-	for index, mask := range masks {
-		manyParts := strings.Split(mask, "*")
-		manyExprs := make([]string, len(manyParts))
-		for mindex, manyPart := range manyParts {
-			oneParts := strings.Split(manyPart, "?")
-			oneExprs := make([]string, len(oneParts))
-			for oindex, onePart := range oneParts {
-				oneExprs[oindex] = regexp.QuoteMeta(onePart)
-			}
-			manyExprs[mindex] = strings.Join(oneExprs, ".")
-		}
-		maskExprs[index] = strings.Join(manyExprs, ".*")
-	}
-	expr := "^" + strings.Join(maskExprs, "|") + "$"
-	rReg, _ = regexp.Compile(expr)
-	return rReg
-}
-
-func (q *quarid) matchMask(mask string) bool {
-	fMask, err := FormatMask(mask)
-	if err != nil {
-		logger.Log.Error(err)
-	}
-	aMasks := []string{}
-	for oper, _ := range q.Opers {
-		operMask, err := FormatMask(oper)
-		if err != nil {
-			logger.Log.Error(err)
-			continue
-		}
-		if operMask.Nick == "*" || operMask.Nick == fMask.Nick {
-			if operMask.User == "*" || operMask.User == fMask.User {
-				aMasks = append(aMasks, operMask.Host)
-			}
-		}
-	}
-	rReg := maskRegexp(aMasks)
-	if rReg.MatchString(fMask.Host) {
-		return true
-	}
-	return false
-}
-
-func (q *quarid) SendPrv(destination, message string) error {
-	response := adapter.Event{
-		Command: irc.IRC_PRIVMSG,
-		Parameters: []string{
-			destination,
-			message,
-		},
-	}
-	if err := q.IRC.Write(&response); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (q *quarid) OPUser(channel, user string) error {
-	response := adapter.Event{
-		Command: irc.IRC_MODE,
-		Parameters: []string{
-			channel,
-			"+o",
-			user,
-		},
-	}
-	if err := q.IRC.Write(&response); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (q *quarid) permissionsMessage(ev *adapter.Event, c adapter.Responder) {
-	message := fmt.Sprintf("%v: bugger off!", getNick(ev.Prefix))
-	if err := q.SendPrv(ev.Parameters[0], message); err != nil {
-		logger.Log.Error(err)
-	}
-}
-
-const CMD_SYM = "!"
-
-func FormatCommand(symbol, input string) ([]string, error) {
-	//check for symbol
-	if len(symbol) < 1 || len(symbol) > 5 {
-		return []string{}, errors.New("invalid symbol")
-	}
-	sym, err := regexp.Compile("^" + symbol + "(.*)$")
-	if err != nil {
-		return []string{}, errors.New("regex: invalid symbol")
-	}
-	if match := sym.FindString(input); len(match) > 0 {
-		return strings.Split(match[1:], " "), nil
-	}
-
-	return []string{}, errors.New("invalid command")
-}
-
-func (q *quarid) checkChanAdmin(ev *adapter.Event) bool {
-	adMasks, _ := q.ChanAdmins[ev.Parameters[0]]
-	if len(adMasks) > 0 {
-		rReg := maskRegexp(adMasks)
-		fMask, err := FormatMask(ev.Prefix)
-		if err != nil {
-			logger.Log.Error(err)
-			return false
-		}
-		return rReg.MatchString(fMask.Host)
-	}
-	return false
-}
-
-func (q *quarid) checkChanPermissions(ev *adapter.Event) bool {
-	opMasks, _ := q.ChanOps[ev.Parameters[0]]
-	adMasks, _ := q.ChanAdmins[ev.Parameters[0]]
-	allMasks := append(opMasks, adMasks...)
-	if len(allMasks) > 0 {
-		rReg := maskRegexp(allMasks)
-		fMask, err := FormatMask(ev.Prefix)
-		if err != nil {
-			logger.Log.Error(err)
-			return false
-		}
-		return rReg.MatchString(fMask.Host)
-	}
-	return false
-}
-
-func (q *quarid) cmdChanOP(ev *adapter.Event, c adapter.Responder) {
-	cmd, err := FormatCommand(CMD_SYM, ev.Parameters[1])
-	if err != nil {
-		return
-	}
-	if strings.ToUpper(cmd[0]) == "OP" {
-		if q.matchMask(ev.Prefix) || q.checkChanPermissions(ev) {
-			if err := q.OPUser(
-				ev.Parameters[0],
-				getNick(ev.Prefix),
-			); err != nil {
-				logger.Log.Error(err)
-			}
-		} else {
-			q.permissionsMessage(ev, c)
-		}
-	}
-}
-
-func (q *quarid) cmdAddAdmin(ev *adapter.Event, c adapter.Responder) {
-	cmd, err := FormatCommand(CMD_SYM, ev.Parameters[1])
-	if err != nil {
-		return
-	}
-	if strings.ToUpper(cmd[0]) == "ADDADMIN" {
-		if q.matchMask(ev.Prefix) || q.checkChanAdmin(ev) {
-			fMask, _ := FormatMask(cmd[1])
-			q.ChanAdmins[ev.Parameters[0]] = append(
-				q.ChanAdmins[ev.Parameters[0]],
-				fMask.Host,
-			)
-		} else {
-			q.permissionsMessage(ev, c)
-		}
-	}
-}
-
-func (q *quarid) cmdDropAdmin(ev *adapter.Event, c adapter.Responder) {
-	cmd, err := FormatCommand(CMD_SYM, ev.Parameters[1])
-	if err != nil {
-		return
-	}
-	if strings.ToUpper(cmd[0]) == "ADDADMIN" {
-		if q.matchMask(ev.Prefix) || q.checkChanAdmin(ev) {
-			fMask, _ := FormatMask(cmd[1])
-			for i, mask := range q.ChanAdmins[ev.Parameters[0]] {
-				if mask == fMask.Host {
-					q.ChanAdmins[ev.Parameters[0]] = append(
-						q.ChanAdmins[ev.Parameters[0]][:i],
-						q.ChanAdmins[ev.Parameters[0]][i+1:]...,
-					)
-				}
-			}
-		} else {
-			q.permissionsMessage(ev, c)
-		}
-	}
-}
-
-func (q *quarid) cmdAddOP(ev *adapter.Event, c adapter.Responder) {
-	cmd, err := FormatCommand(CMD_SYM, ev.Parameters[1])
-	if err != nil {
-		return
-	}
-	if strings.ToUpper(cmd[0]) == "ADDOP" {
-		if q.matchMask(ev.Prefix) || q.checkChanAdmin(ev) {
-			fMask, _ := FormatMask(cmd[1])
-			q.ChanOps[ev.Parameters[0]] = append(
-				q.ChanOps[ev.Parameters[0]],
-				fMask.Host,
-			)
-		} else {
-			q.permissionsMessage(ev, c)
-		}
-	}
-}
-
-func (q *quarid) cmdDropOp(ev *adapter.Event, c adapter.Responder) {
-	cmd, err := FormatCommand(CMD_SYM, ev.Parameters[1])
-	if err != nil {
-		return
-	}
-	if strings.ToUpper(cmd[0]) == "DROPOP" {
-		if q.matchMask(ev.Prefix) || q.checkChanAdmin(ev) {
-			fMask, _ := FormatMask(cmd[1])
-			for i, mask := range q.ChanOps[ev.Parameters[0]] {
-				if mask == fMask.Host {
-					q.ChanOps[ev.Parameters[0]] = append(
-						q.ChanOps[ev.Parameters[0]][:i],
-						q.ChanOps[ev.Parameters[0]][i+1:]...,
-					)
-				}
-			}
-		} else {
-			q.permissionsMessage(ev, c)
-		}
-	}
-}
-
-func (q *quarid) handleQuit(ev *adapter.Event, c adapter.Responder) {
+func (q *Quarid) handleQuit(ev *adapter.Event, c adapter.Responder) {
 	if len(ev.Parameters) > 0 && ev.Parameters[0] == "quit" {
 		logger.Log.Warnf("Recieved Quit Command From Server")
 		logger.Log.Warnf("Shutting Down Bot")
@@ -396,7 +101,7 @@ func (q *quarid) handleQuit(ev *adapter.Event, c adapter.Responder) {
 	}
 }
 
-func (q *quarid) Connect() error {
+func (q *Quarid) Connect() error {
 	err := q.IRC.Connect(q.Config.GetString("irc.server"))
 	if err != nil {
 		return err
@@ -406,19 +111,6 @@ func (q *quarid) Connect() error {
 		[]adapter.Filter{irc.CommandFilter{Command: irc.IRC_RPL_MYINFO}},
 		q.joinChan,
 	)
-	prvFuncs := []adapter.HandlerFunc{
-		q.cmdChanOP,
-		q.cmdAddOP,
-		q.cmdDropOp,
-		q.cmdAddAdmin,
-		q.cmdDropAdmin,
-	}
-	for _, hF := range prvFuncs {
-		q.IRC.Handle(
-			[]adapter.Filter{irc.CommandFilter{Command: irc.IRC_PRIVMSG}},
-			hF,
-		)
-	}
 
 	q.IRC.Handle(
 		[]adapter.Filter{irc.CommandFilter{Command: irc.IRC_ERROR}},
@@ -429,19 +121,19 @@ func (q *quarid) Connect() error {
 	return err
 }
 
-func (q *quarid) Disconnect() {
+func (q *Quarid) Disconnect() {
 	q.IRC.Disconnect()
 }
 
-func (q *quarid) Plugins() []plugin.Plugin {
+func (q *Quarid) Plugins() []plugin.Plugin {
 	return q.plugins
 }
 
-func (q *quarid) VMs() map[string]vm.VM {
+func (q *Quarid) VMs() map[string]vm.VM {
 	return q.vms
 }
 
-func (q *quarid) joinChan(
+func (q *Quarid) joinChan(
 	ev *adapter.Event,
 	c adapter.Responder,
 ) {
