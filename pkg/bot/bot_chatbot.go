4fbebbad2553606ac5cf40e71a9b4448bda1c7b7
lirancohen <lirancohen@me.com>
2015-11-14 13:25:56 -0500

    IRC Bot Services start

--- pkg/bot/bot_chatbot.go
+++ pkg/bot/bot_chatbot.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"strings"
+
 	"github.com/enmand/quarid-go/pkg/adapter"
 	"github.com/enmand/quarid-go/pkg/logger"
 	//"github.com/enmand/quarid-go/pkg/config"
@@ -12,7 +14,8 @@ type MsgService struct {
 
 type Service interface {
 	Name() string
-	Handler() MsgHandler
+	Description() string
+	Commands() []Command
 }
 
 type PrvMsg struct {
@@ -21,7 +24,7 @@ type PrvMsg struct {
 	Message string
 }
 
-type MsgHandler func(*PrvMsg, adapter.Responder)
+type MsgHandler func(*Cmd, adapter.Responder)
 
 func (ms *MsgService) initialize(services ...Service) adapter.HandlerFunc {
 	ms.Services = make(map[string]MsgHandler)
@@ -32,12 +35,65 @@ func (ms *MsgService) initialize(services ...Service) adapter.HandlerFunc {
 	return func(ev *adapter.Event, c adapter.Responder) {}
 }
 
+type Command struct {
+	Name        string
+	Description string
+	Parameters  []map[string][]string
+	Handler     MsgHandler
+}
+
+type Cmd struct {
+	Name       string
+	Parameters []string
+}
+
 type ChanBot struct {
-	Name string
+	Name        string
+	Description string
+	Commands    []Command
 }
 
 func (cb *ChanBot) Name() string {
 	return cb.Name
+}
+
+func (cb *ChanBot) Description() string {
+	return cb.Description
+}
+
+func (cb *ChanBot) Commands() (commands []Command) {
+	return cb.Commands
+	cmdOp := Command{
+		Name:        "OP",
+		Description: "OP yourself or another user within a channel",
+		Parameters: map[string][]string{
+			"NICK": {
+				"(optional) The Nick of the use you would like to OP, leave blank to OP yourself",
+			},
+			"CHANNEL": {
+				"(optional) If sent in a Private Message please include the target channel",
+			},
+		},
+	}
+	checkChannel = func(s string) bool {
+		if len(s) > 0 && s[0:1] == "#" {
+			return true
+		}
+		return false
+	}
+	cmdOp.Handler = func(*Cmd, adapter.Responder) {
+		if Cmd.Name == "OP" {
+			switch len(Cmd.Parameters) {
+			case 0:
+				if !checkChannel(Cmd.Parameters[0]) {
+				}
+			case 1:
+			case 2:
+			default:
+			}
+		}
+	}
+	return
 }
 
 //func (pm *PrvMsg) cmdChanOP() {
